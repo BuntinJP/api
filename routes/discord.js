31dc63fc1a6952af3f53c7703f1042db0b012bf3
Buntin-Synthia <aokitakumi72@gmail.com>
2022-11-27 04:48:17 +0900

    torima

--- routes/discord.js
+++ routes/discord.js
@@ -1,18 +1,24 @@
-let express = require("express");
-const bodyParser = require("body-parser");
+let express = require('express');
+const bodyParser = require('body-parser');
 let router = express.Router();
+const fs = require('fs');
 
 router.use(bodyParser.json());
-router.get("/", function (req, res, next) {
-    res.render("apiExp", { title: "discord", api: "discord" });
+router.get('/', function (req, res, next) {
+    res.render('apiExp', { title: 'discord', api: 'discord' });
 });
 
+router.get('/tokens', function (req, res, next) {
+    const data = JSON.parse(fs.readFileSync('./setting.json', 'utf8'));
+    const tokens = data.tokens;
+    sendJson(res, tokens);
+});
 
-router.post("/", function (req, res, next) {
+router.post('/', function (req, res, next) {
     let data = req.body;
     console.log(data);
     console.log(data.test);
-    res.send("discord");
+    res.send('discord');
 });
 
 module.exports = router;
