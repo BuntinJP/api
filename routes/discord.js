9bb8ff80f844ca7b14f3d133c5a8926b1ca1ee54
Buntin-Synthia <aokitakumi72@gmail.com>
2022-11-27 14:33:50 +0900

    discord.jsの開発大詰め

--- routes/discord.js
+++ routes/discord.js
@@ -11,16 +11,40 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/tokens', function (req, res, next) {
-    setting().then((data) => {
-        res.send(data);
-    }).catch((err) => {
-        res.send(err);
-    });
+    setting()
+        .then((data) => {
+            let setting = JSON.parse(data);
+            let tokens = setting.tokens;
+            res.send(tokens);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
-router.post("/tokens", function (req, res, next) {
-    res.send("ok");
-})
+router.post('/tokens', function (req, res, next) {
+    let data = req.body;
+    if (data.token) {
+        setting().then((setting) => {
+            let localSetting = JSON.parse(setting);
+            let tokensLength = localSetting.tokens.length;
+            localSetting.tokens[tokensLength] = {
+                token: data.token,
+            };
+            getChannelName(data.token);
+            let outSetting = JSON.stringify(localSetting);
+            fs.writeFile('setting.json', outSetting, (err) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send('success');
+                }
+            });
+        });
+    } else {
+        res.send('token not found');
+    }
+});
 
 router.post('/', function (req, res, next) {
     let data = req.body;
@@ -30,23 +54,33 @@ router.post('/', function (req, res, next) {
 
 async function sendData(data) {
     let response = await axios.post(data.token, { content: data.content });
-    fs.writeFile("./logs/response.json", JSON.stringify(decycle(response)), function (err) {
-        if (err) {
-            return console.log(err);
+    fs.writeFile(
+        './logs/response.json',
+        JSON.stringify(decycle(response)),
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
         }
-    });
+    );
     return response;
+}
+
+async function getChannelName(token) {
+    let response = await axios.get(token);
+    response = JSON.stringify(decycle(response));
+    console.log(response.keys());
 }
 
 const setting = () => {
     return new Promise((resolve, reject) => {
         fs.readFile('./setting.json', 'utf8', (err, data) => {
             if (err) {
-                reject("setting.json not found");
+                reject('setting.json not found');
             }
             resolve(data);
         });
     });
-}
+};
 
 module.exports = router;
